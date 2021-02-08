c6cb34a78eb7d470a446983ed2972d1abfc8bf48
sjahn <asj214@naver.com>
2021-02-08 23:16:17 +0900

    materialpro-vuetify-admin-lite

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
-import App from '@/App.vue'
-import router from '@/router'
-import store from '@/store'
+import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import router from './router'
+import store from './store'
+import './plugins/base'
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    vuetify,
+    store,
+    router,
+    render: h => h(App)
 }).$mount('#app')
